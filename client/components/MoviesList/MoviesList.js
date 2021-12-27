7f0778191d62dbcd6875095988a70bcd587e9954
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-12-26 22:49:00 -0800

    added redux toolkit

---
+++ client/components/MoviesList/MoviesList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./MoviesList.module.css";
+
+const MoviesList = () => {
+  return (
+    <ul className={styles["list"]}>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
+  );
+};
+
+export default MoviesList;
